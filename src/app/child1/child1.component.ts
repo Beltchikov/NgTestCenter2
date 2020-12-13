6a9c096903fba91c61c57e656256999786e97f47
Jogger <beltchikov@gmx.de>
2020-12-13 20:45:23 +0100

    Parent to child

--- src/app/child1/child1.component.ts
+++ src/app/child1/child1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child1',
@@ -9,7 +9,10 @@ export class Child1Component implements OnInit {
 
   constructor() { }
 
+  @Input() item:string;
+
   ngOnInit(): void {
+    this.item = "Waiting for the parent...";
   }
 
 }
