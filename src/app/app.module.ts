fd116904ff0991509dc79f58a052e3d1d58a5288
Jogger <beltchikov@gmx.de>
2020-12-13 13:36:40 +0100

    Parent and Child components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { DropdownComponent } from './dropdown/dropdown.component';
 import { ModalComponent } from './modal/modal.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { GridComponent } from './grid/grid.component';
+import { Parent1Component } from './parent1/parent1.component';
+import { Child1Component } from './child1/child1.component';
+import { Parent2Component } from './parent2/parent2.component';
+import { Child2Component } from './child2/child2.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { GridComponent } from './grid/grid.component';
     DropdownComponent,
     ModalComponent,
     NavbarComponent,
-    GridComponent
+    GridComponent,
+    Parent1Component,
+    Child1Component,
+    Parent2Component,
+    Child2Component
   ],
   imports: [
     BrowserModule,
